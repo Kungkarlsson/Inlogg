7b8b8646eccf4eab86c5caee87ab672bed3d1710
Lärande I Arbete 1 <lia1@ad.impleoit.se>
2017-06-07 11:08:04 +0200

    ändra kommentarerna

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-//skapar min connection ($conn) till min databas (mydatabase).
+//skapar min connection ($conn) mha inbygda metoden "mysqli_connect", till min databas (mydatabase).
 $conn = mysqli_connect("localhost", "root", "", "mydatabase");
 if (!$conn){
 	//ifall vi inte hittar en connection använder jag "die" för att döda sidan och ge ett felmeddelande.
