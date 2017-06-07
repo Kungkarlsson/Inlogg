a5a5654a76541fb39ba47ec10c1f74fc03f9b3dc
Lärande I Arbete 1 <lia1@ad.impleoit.se>
2017-06-07 10:23:56 +0200

    LoginForm-Design skapad, startsidan

--- startpage.php
+++ startpage.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <link rel="stylesheet" type="text/css" href="mystyle.css">
+  <meta charset="UTF-8">
 </head>
 <body>
-
+<?php
+//Skapar min startsida, min login-form. Post-method för att kunna skicka info till min databas. Har också addat "link" till stylesheet för att fixa designen.
+?>
+    <form action="login.php" method="post">
+      <input type="text" name="username" placeholder="U s e r n a m e"/><br>
+      <input type="password" name="password" placeholder="P a s s w o r d"/><br>
+      <button>login</button>
+    </form>
 </body>
 </html>
