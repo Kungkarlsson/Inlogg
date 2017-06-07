a73ef82e983ad6fd27915a4bf6b835cfe01cdcc7
Lärande I Arbete 1 <lia1@ad.impleoit.se>
2017-06-07 10:36:03 +0200

    Skapar min connection till databas, i en seperat fil för snyggare kod

--- connect.php
+++ connect.php
@@ -1,1 +1,8 @@
-connect
+<?php
+//skapar min connection ($conn) till min databas (mydatabase).
+$conn = mysqli_connect("localhost", "root", "", "mydatabase");
+if (!$conn){
+	//ifall vi inte hittar en connection använder jag "die" för att döda sidan och ge ett felmeddelande.
+ 	die("Connection failed: ". mysqli_connect_error());
+ }
+?>
