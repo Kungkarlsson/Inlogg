eb6dffde7037ab5dd62cd480ce0ef483dcdab801
Lärande I Arbete 1 <lia1@ad.impleoit.se>
2017-06-07 10:29:22 +0200

    La till divar och klassnamnen som behövdes för att få css att funka

--- startpage.php
+++ startpage.php
@@ -8,10 +8,15 @@
 <?php
 //Skapar min startsida, min login-form. Post-method för att kunna skicka info till min databas. Har också addat "link" till stylesheet för att fixa designen.
 ?>
-    <form action="login.php" method="post">
-      <input type="text" name="username" placeholder="U s e r n a m e"/><br>
-      <input type="password" name="password" placeholder="P a s s w o r d"/><br>
+<div class="login-page">
+  <div class="form">
+    <form action="login.php" method="post" login-form">
+      <input type="text" name="username" placeholder="U s e r n a m e"/>
+      <input type="password" name="password" placeholder="P a s s w o r d"/>
       <button>login</button>
     </form>
+  </div>
+</div>
+
 </body>
 </html>
