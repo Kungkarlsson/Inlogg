4db707948c79cc41a9f8123b7862e143552e2af8
Lärande I Arbete 1 <lia1@ad.impleoit.se>
2017-06-07 10:55:52 +0200

    Påbörjat login filen där logiken ska finnas. Alltså ifall username och password stämmer och finns i databasen

--- login.php
+++ login.php
@@ -1,1 +1,10 @@
-login
+<?php
+//använder "include" för att inkludera min connect.php. Alltså min connection till databasen.
+include 'connect.php';
+
+//Sätter två varibler ($username, $password) till att vara username och password från min databas mha POST.
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+//Sätter en variabel ($sql] mha att använda SELECT kommando på mitt table "persons" för att få ut användarna. 
+$sql = "SELECT * FROM persons WHERE username = '$username' AND password = '$password'";
