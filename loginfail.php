c04caccb7e89d1fbcb092a178286e1d3793d0a9c
Lärande I Arbete 1 <LIA1@ad.impleoit.se>
2017-06-08 09:30:26 +0200

    Gjorde en ny fil för en ny sida ifall inloggsuppgifter är fel

---
+++ loginfail.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="mystyle.css">
+  <meta charset="UTF-8">
+</head>
+<body>
+<?php
+//En sida ifall man skriver in fel uppgifter bara för snygghetens skull. Använder samma css som vid loginform.
+?>
+<div class="login-page">
+  <div class="form">
+    <form>
+     <h3>Username or Password was incorrect, go back and try again!</h3>
+    </form>
+  </div>
+</div>
+</body>
+</html>
