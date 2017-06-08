9c9ce7008cd3a1778068d201f238fddf7e92093e
Lärande I Arbete 1 <LIA1@ad.impleoit.se>
2017-06-08 09:32:35 +0200

    ändrade styling med samma css samt kommentarer

--- loggedin.php
+++ loggedin.php
@@ -1,1 +1,19 @@
-secret logged in page
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="mystyle.css">
+  <meta charset="UTF-8">
+</head>
+<body>
+<?php
+//Min hemliga sida som visas när man är inloggad. Använder samma css som innan.
+?>
+<div class="login-page">
+  <div class="form">
+    <form login-form">
+    <h3>This is a secret page, your logged in!</h3>
+    </form>
+  </div>
+</div>
+</body>
+</html>
