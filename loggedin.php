fb1b6c95a0a81eea660b39973df91da0f0b8ccda
Lärande I Arbete 1 <LIA1@ad.impleoit.se>
2017-06-08 09:59:23 +0200

    Lagt till en logout knapp som skickar tillbaka dig till startsidan samt gör session_destroy för att startar om en ny session

--- loggedin.php
+++ loggedin.php
@@ -6,12 +6,22 @@
 </head>
 <body>
 <?php
+session_start();
+//session_start() för att ha igång min session.
 //Min hemliga sida som visas när man är inloggad. Använder samma css som innan.
 ?>
 <div class="login-page">
   <div class="form">
-    <form login-form">
+    <form method="POST" action="" login-form">
     <h3>This is a secret page, your logged in!</h3>
+	<button name="button1">Logout</button>
+	<?php
+		if (isset($_POST['button1'])) 
+		{ 
+   			session_destroy();
+   			header("Location:startpage.php"); 
+		}  
+	?>
     </form>
   </div>
 </div>
