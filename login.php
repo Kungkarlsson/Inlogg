b79e7498b4afd197ba585e404b57676516bb81ac
Lärande I Arbete 1 <lia1@ad.impleoit.se>
2017-06-08 09:04:35 +0200

    Lade till logiken för att kolla om användaruppgifter stämmer överens

--- login.php
+++ login.php
@@ -6,5 +6,17 @@ include 'connect.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-//Sätter en variabel ($sql] mha att använda SELECT kommando på mitt table "persons" för att få ut användarna. 
+//Sätter en variabel ($sql) mha att använda SELECT kommando på mitt table "persons" för att få ut användarna. 
 $sql = "SELECT * FROM persons WHERE username = '$username' AND password = '$password'";
+
+//kör en query på min databas och sätter $result till värdet.
+$result = mysqli_query($conn, $sql);
+
+//kör mysqli_fetch_assoc för att få ett resultat som en array av en row i databasen och sätter det till $row. och jämnför med $result.
+//om $row skiljer sig från $result är det fel lösenord och felmeddelande skickas. annars "your logged in"
+if (!$row = mysqli_fetch_assoc($result)) {
+ 	echo "Your username or password is incorrect!";
+ } else {
+ 	echo "Your logged in!";
+ }
+?>
