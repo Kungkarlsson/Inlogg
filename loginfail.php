1dd76abb52da0ce5b803ef26d55093fa6b2b5568
Lärande I Arbete 1 <LIA1@ad.impleoit.se>
2017-06-08 09:59:43 +0200

    lagt till session_Start

--- loginfail.php
+++ loginfail.php
@@ -6,6 +6,8 @@
 </head>
 <body>
 <?php
+session_start();
+//session_start() för att ha igång min session.
 //En sida ifall man skriver in fel uppgifter bara för snygghetens skull. Använder samma css som vid loginform.
 ?>
 <div class="login-page">
